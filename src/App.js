04e315c1006c28dcceb41e3c99e16274c8fc10f4
sd19941508 <sd19941508@gmail.com>
2024-02-14 10:22:59 -0800

    feat: basic routing functionality

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+
 import "./App.css";
+
+import Header from "./example/Header";
+import Home from "./example/Home";
 import Example from "./example/Example";
 import LoginForm from "./example/LoginForm";
 
 function App() {
   return (
-    <div>
-      {/* <h1 className="text-3xl font-bold underline">Hello world!</h1> */}
-      <Example />
-      <br />
-      <hr />
-      <br />
-      <LoginForm />
-    </div>
+    <Router>
+      <Header />
+      <div>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/product" element={<Example />} />
+          <Route path="/login/:userId" element={<LoginForm />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/example/Home.js
@@ -0,0 +1,18 @@
+export default function Home() {
+  return (
+    <div className="bg-white">
+      <div className="relative isolate py-64 lg:px-8">
+        <div className="text-center">
+          <h1 className="text-6xl font-bold tracking-tight text-gray-900 sm:text-6xl">
+            Welcome to landing page...
+          </h1>
+          <p className="mt-6 text-lg leading-8 text-gray-600">
+            Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui
+            lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat
+            fugiat aliqua.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
