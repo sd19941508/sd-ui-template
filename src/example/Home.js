524928a1b7202f034c8ab705fcc4a546533f4b67
sd19941508 <sd19941508@gmail.com>
2024-02-15 02:56:57 -0800

    install some lib

--- src/example/Home.js
+++ src/example/Home.js
@@ -13,7 +13,7 @@ export default function Home() {
           <h1 className="text-6xl font-bold tracking-tight text-gray-900 sm:text-6xl">
             Welcome to landing page...
           </h1>
-          <p className="mt-6 text-lg leading-8 text-gray-600">
+          <p className="mt-6 text-lg leading-8 text-gray-600 text-sd">
             Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui
             lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat
             fugiat aliqua.
